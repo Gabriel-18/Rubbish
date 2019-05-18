85911edba06fc1467b22ad42ff95dadb828ae4b5
libin <644050173@qq.com>
2019-05-18 16:26:24 +0800

    40. Combination Sum II

--- LeetCode/src/leetcode/Combinations.java
+++ LeetCode/src/leetcode/Combinations.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Combinations {
     /**
      * https://leetcode.com/problems/combinations/
+     * 77. Combinations
      * @param n
      * @param k
      * @return
