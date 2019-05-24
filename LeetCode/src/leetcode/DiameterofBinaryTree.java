7489807202a064130390e60baf3b44a13d31ca30
libin <644050173@qq.com>
2019-05-24 14:58:54 +0800

    543. Diameter of Binary Tree

---
+++ LeetCode/src/leetcode/DiameterofBinaryTree.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class DiameterofBinaryTree {
+    /**
+     * 543. Diameter of Binary Tree
+     * https://leetcode.com/problems/diameter-of-binary-tree/
+     */
+    private int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max;
+    }
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leafDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        // 底层和上层比较
+        max = Math.max(max, leafDepth + rightDepth);
+        return Math.max(leafDepth, rightDepth) + 1;
+    }
+}
