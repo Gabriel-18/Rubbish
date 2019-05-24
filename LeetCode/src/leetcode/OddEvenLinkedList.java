902188514fe35e552bc57a5390d60746b9873a22
libin <644050173@qq.com>
2019-05-24 13:57:51 +0800

    328. Odd Even Linked List

---
+++ LeetCode/src/leetcode/OddEvenLinkedList.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class OddEvenLinkedList {
+    /**
+     * https://leetcode.com/problems/odd-even-linked-list/description/
+     * 328. Odd Even Linked List
+     * 画图
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
