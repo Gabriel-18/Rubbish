5f40d34f4ada23a7b6444511d5c41d20af9479a8
libin <644050173@qq.com>
2019-05-26 15:08:10 +0800

    687. Longest Univalue Path

---
+++ LeetCode/src/leetcode/LongestUnivaluePath.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class LongestUnivaluePath {
+    /**
+     * 687. Longest Univalue Path
+     * https://leetcode.com/problems/longest-univalue-path/
+     * 与543. Diameter of Binary Tree 有点相似
+     * @param root
+     * @return
+     */
+    private int path = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return path;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        int leafPath = root.left != null && root.left.val == root.val ? left + 1: 0;
+        int rightPath = root.right != null && root.right.val == root.val ? right + 1: 0;
+
+        path = Math.max(path, leafPath + rightPath);
+        return Math.max(leafPath, rightPath);
+
+    }
+}
