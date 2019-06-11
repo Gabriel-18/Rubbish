6358ce0ad851011fa6687218df82bfa08603b7c4
libin <644050173@qq.com>
2019-06-11 13:59:26 +0800

    565. Array Nesting

---
+++ LeetCode/src/leetcode/ArrayNesting.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class ArrayNesting {
+    /***
+     * https://leetcode.com/problems/array-nesting/
+     * 565. Array Nesting
+     * @param nums
+     * @return
+     */
+    public int arrayNesting(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int cnt = 0;
+            // 起点
+            for (int j = i; nums[j] != -1; ) {
+                cnt ++;
+                int t = nums[j];
+                // 标记已经被访问过
+                nums[j] = -1;
+                j = t;
+            }
+            max = Math.max(max, cnt);
+        }
+        return max;
+    }
+}
