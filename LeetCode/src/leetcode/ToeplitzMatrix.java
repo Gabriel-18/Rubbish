c30d3aacc0bbf6426c3c7552e20b73c16635af2f
libin <644050173@qq.com>
2019-06-11 13:33:19 +0800

    128. Longest Consecutive Sequence and add some comments

--- LeetCode/src/leetcode/ToeplitzMatrix.java
+++ LeetCode/src/leetcode/ToeplitzMatrix.java
@@ -27,4 +27,17 @@ public class ToeplitzMatrix {
         }
         return true;
     }
+
+    public boolean isToeplitzMatrix2(int[][] matrix) {
+        Map<Integer, Integer> groups = new HashMap<>();
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                //比较上一个对角线上的元素和当前的值
+                if (r > 0 && c > 0 && matrix[r - 1][c - 1] != matrix[r][c]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
