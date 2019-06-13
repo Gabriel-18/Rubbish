db8c0b1daa8abd9266acbc647fd17bf3239f6c6f
libin <644050173@qq.com>
2019-06-13 21:22:13 +0800

    345. Reverse Vowels of a String

---
+++ LeetCode/src/leetcode/Reversevowelsofastring.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Reversevowelsofastring {
+    private final static HashSet<Character> vowels = new HashSet<>(
+            Arrays.asList('a','e','i','o','u','A','E','I','O','U')
+    );
+    /**
+     * 345. Reverse Vowels of a String
+     * https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+     * @param s
+     * @return
+     */
+    public String reverseVowels(String s) {
+        int i  = 0, j = s.length() - 1;
+        char[] result = new char[s.length()];
+        while (i <= j) {
+            char ci = s.charAt(i);
+            char cj = s.charAt(j);
+
+            if (!vowels.contains(ci)) {
+                result[i++] = ci;
+            } else if (!vowels.contains(cj)) {
+                result[j--] = cj;
+            } else {
+                // 交换
+                result[i++] = cj;
+                result[j--] = ci;
+            }
+        }
+        return new String(result);
+    }
+}
