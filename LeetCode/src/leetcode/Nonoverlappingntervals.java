b69d34ca3c76092d93d99564722935f702347896
libin <644050173@qq.com>
2019-06-17 11:55:02 +0800

    128. Longest Consecutive Sequence

--- LeetCode/src/leetcode/Nonoverlappingntervals.java
+++ LeetCode/src/leetcode/Nonoverlappingntervals.java
@@ -19,7 +19,15 @@ public class Nonoverlappingntervals {
         if (intervals.length == 0) {
             return 0;
         }
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        //在每次选择中，区间的结尾最为重要，选择的区间结尾越小，留给后面的区间的空间越大，
+        // 那么后面能够选择的区间个数也就越大。
+        //Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
         int cnt = 1;
         int end = intervals[0][1];
 
