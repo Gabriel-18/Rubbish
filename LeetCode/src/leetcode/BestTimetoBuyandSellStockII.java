14aa80de2910bb2f1cdaae75a822288ff6d17648
libin <644050173@qq.com>
2019-06-18 20:58:37 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ LeetCode/src/leetcode/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class BestTimetoBuyandSellStockII {
+    /**
+     * 122. Best Time to Buy and Sell Stock II
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+     * 拿前后两天做差,有的赚就交易
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
