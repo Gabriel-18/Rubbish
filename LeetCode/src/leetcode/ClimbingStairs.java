d65db751c874a7f6af1efb7e772369cad897677b
libin <644050173@qq.com>
2019-06-21 09:46:45 +0800

    70. Climbing Stairs

--- LeetCode/src/leetcode/ClimbingStairs.java
+++ LeetCode/src/leetcode/ClimbingStairs.java
@@ -3,6 +3,7 @@ package leetcode;
 public class ClimbingStairs {
     /**
      * https://leetcode.com/problems/climbing-stairs/
+     * 70. Climbing Stairs
      * 本质就是斐波那契数列
      * 为啥我这个会超时呢????????
      * @param n
@@ -10,20 +11,24 @@ public class ClimbingStairs {
      */
 
     public static int climbStairs(int n) {
-        int[] dp = new int[Integer.MAX_VALUE];
-        if (n <= 2) {
-            return n;
+        if (n == 1) {
+            return 1;
         }
-        if (dp[n] != 0) {
-            return dp[n];
-        } else {
-            dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
-            return dp[n];
+
+        int[] dp = new int[n + 1];
+        // 边界
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
+
+        return dp[n];
     }
 
     /**
-     * 画图
+     * 一样是斐波那契数列
      * @param n
      * @return
      */
@@ -37,6 +42,27 @@ public class ClimbingStairs {
             oneStep = res;
         }
         return res;
+    }
+
+    /**
+     * 利用斐波那契数列规律
+     * 感觉不算动态规划
+     * @param n
+     * @return
+     */
+    public int climbStairs3(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        // dp[i - 1] dp[i - 2]
+        int pre2 = 1, pre1 = 2;
+        for (int i = 2; i < n; i++) {
+            int cur = pre1 + pre2;
+            // 前进
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
     }
     public static void main(String[] args) {
         System.out.println(climbStairs2(38));
