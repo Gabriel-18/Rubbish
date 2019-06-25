dc83063814fda74431849ccc1ff600acf5f98411
libin <644050173@qq.com>
2019-06-25 20:27:59 +0800

    add some comments

--- LeetCode/src/leetcode/MinimumPathSum.java
+++ LeetCode/src/leetcode/MinimumPathSum.java
@@ -4,7 +4,14 @@ public class MinimumPathSum {
     /**
      * 64. Minimum Path Sum
      * https://leetcode.com/problems/minimum-path-sum/description/
-     * 不懂
+     * dp[i][j]表示从00位置到ij位置的最小路径和
+     * 从上往下走
+     * dp[i][j] = dp[i-1][[j] + m[i][j]
+     * 从左往右走
+     * dp[i][j] = dp[i][[j-1] + m[i][j]
+     * 有两个边界
+     * 当i=0时只能向右走 即从左到右
+     * 当j=0时只能向下走 即从上到下
      * @param grid
      * @return
      */
@@ -19,13 +26,14 @@ public class MinimumPathSum {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (j == 0) {
-                    dp[j] = dp[j]; // 只能从上侧走到该位置
+                    // 只能从上侧走到该位置
+                    dp[j] = dp[j] + grid[i][j];
                 } else if (i == 0){
-                    dp[j] = dp[j - 1]; // 只能从左侧走到该位置
+                    // 只能从左侧走到该位置
+                    dp[j] = dp[j - 1] + grid[i][j];
                 } else {
-                    dp[j] = Math.min(dp[j - 1], dp[j]);
+                    dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
                 }
-                dp[j] += grid[i][j];
             }
         }
         return dp[n - 1];
