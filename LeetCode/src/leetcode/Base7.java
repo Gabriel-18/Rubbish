3647997b37f04a6a01d8a5c586d5add5d80ad764
Kelper <644050173@qq.com>
2019-07-12 21:44:48 +0800

    504. Base 7

---
+++ LeetCode/src/leetcode/Base7.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Base7 {
+    /**
+     * 504. Base 7
+     * https://leetcode.com/problems/base-7/description/
+     * @param num
+     * @return
+     */
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+
+        boolean isNegative = num < 0;
+        if (isNegative) {
+            num = -num;
+        }
+
+        while (num > 0) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+
+        String ret = sb.reverse().toString();
+
+        // recover its operator if it is a negative
+        return isNegative ? "-" + ret : ret;
+    }
+}
