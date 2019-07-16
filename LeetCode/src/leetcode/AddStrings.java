9f59b540e1e26300d4acf2d9fdb5caa1298b887b
Kelper <644050173@qq.com>
2019-07-16 15:17:03 +0800

    415. Add Strings

---
+++ LeetCode/src/leetcode/AddStrings.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class AddStrings {
+    /**
+     * https://leetcode.com/problems/add-strings/description/
+     * 415. Add Strings
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String addStrings(String num1, String num2) {
+        StringBuilder str = new StringBuilder();
+        int carry = 0, i = num1.length() - 1, j = num2.length() - 1;
+        while (carry == 1 || i >= 0 || j >= 0) {
+            int x = i < 0 ? 0 : num1.charAt(i--) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j--) - '0';
+
+            str.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        return str.reverse().toString();
+    }
+}
