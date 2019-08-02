6d2e473d7d626e847d6701f08d72dbccc7f22c95
Kelper <644050173@qq.com>
2019-08-02 21:07:56 +0800

    add some new comments

--- LeetCode/src/leetcode/MergeSortedArray.java
+++ LeetCode/src/leetcode/MergeSortedArray.java
@@ -4,6 +4,7 @@ public class MergeSortedArray {
     /**
      * 88. Merge Sorted Array
      * https://leetcode.com/problems/merge-sorted-array/description/
+     * 这个算法就是归并排序的部分
      * 两个数组都是有序
      * 一共分成四种情况
      * 第一个数组处理完
@@ -20,13 +21,13 @@ public class MergeSortedArray {
         int indexMerge = m + n - 1;
 
         while (index1 >= 0 || index2 >= 0) {
-            if (index1 < 0) {
+            if (index1 < 0) { // 只剩下index2的情况
                 nums1[indexMerge--] = nums2[index2--];
-            } else if (index2 < 0) {
+            } else if (index2 < 0) { // 只剩下index1的情况
                 nums1[indexMerge--] = nums1[index1--];
-            } else if (nums1[index1] > nums2[index2]) {
+            } else if (nums1[index1] > nums2[index2]) { // 正常人的入口
                 nums1[indexMerge--] = nums1[index1--];
-            } else {
+            } else { // 正常人的入口
                 nums1[indexMerge--] = nums2[index2--];
             }
         }
