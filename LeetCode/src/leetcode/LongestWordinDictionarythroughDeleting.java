58078858d82313f4251aa361120f9bd3180dde9b
Kelper <644050173@qq.com>
2019-08-03 21:00:09 +0800

    add some new comments

--- LeetCode/src/leetcode/LongestWordinDictionarythroughDeleting.java
+++ LeetCode/src/leetcode/LongestWordinDictionarythroughDeleting.java
@@ -13,11 +13,21 @@ public class LongestWordinDictionarythroughDeleting {
     public String findLongestWord(String s, List<String> d) {
         String longestWord = "";
         for (String target : d) {
+
             int l1 = longestWord.length(), l2 = target.length();
-            // 保证字典序并且比当前的大 一样长的时候考虑字典序 < 0 表示字典序小 a - c类似
+            //第一个条件是 find the longest string in the dictionary
+            // 保证字典序并且比当前的大
+            // 一样长的时候考虑字典序 < 0
+            // 表示字典序小 a - c类似
+            // 针对第二个条件
+            //  If there are more than one possible results,
+            //  return the longest word with the smallest lexicographical order.
             if (l1 > l2 || (l1 == l2 && longestWord.compareTo(target) < 0)) {
                 continue;
             }
+
+            // 符合字串即替代
+            //核心代码
             if (isSubstr(s, target)) {
                 longestWord = target;
             }
