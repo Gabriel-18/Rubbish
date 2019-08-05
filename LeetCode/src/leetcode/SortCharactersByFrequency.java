05131348b092f3957bca5245063eb776121ec4e7
Kelper <644050173@qq.com>
2019-08-05 21:00:30 +0800

    add some new comments

--- LeetCode/src/leetcode/SortCharactersByFrequency.java
+++ LeetCode/src/leetcode/SortCharactersByFrequency.java
@@ -19,25 +19,28 @@ public class SortCharactersByFrequency {
             frequencyForNum.put(c, frequencyForNum.getOrDefault(c, 0) + 1);
         }
 
-        // 开始桶统计
+        // 开始统计
         List<Character>[] frequencyBucket = new ArrayList[s.length() + 1];
-        // 将频率相同的放在一起
+
         for (char c : frequencyForNum.keySet()) {
+            // 将频率相同的放在一个桶吧
             int f = frequencyForNum.get(c);
+            // 第一次处理时
             if (frequencyBucket[f] == null) {
                 frequencyBucket[f] = new ArrayList<>();
             }
-            // 相同的
+            // 将相同频率的添加到一起
             frequencyBucket[f].add(c);
         }
-        // 逆序
+        // 逆序 因为要从高频开始输出
         StringBuilder str = new StringBuilder();
         for (int i = frequencyBucket.length - 1; i >= 0; i--) {
+            // 忽略不存在的
             if (frequencyBucket[i] == null) {
                 continue;
             }
             for (char c : frequencyBucket[i]) {
-                for (int j =- 0; j < i; j++) {
+                for (int j = 0; j < i; j++) {
                     str.append(c);
                 }
             }
