3cfcd3f0dd46dbb441fda81c768b131b532a517c
Kelper <644050173@qq.com>
2019-08-06 21:42:08 +0800

    add some new comments

--- LeetCode/src/leetcode/AssignCookies.java
+++ LeetCode/src/leetcode/AssignCookies.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class AssignCookies {
     /**
+     * 贪心算法
+     * 保证每次操作都是局部最优的，并且最后得到的结果是全局最优的
      *455. Assign Cookies
      * https://leetcode.com/problems/assign-cookies/description/
      * @param g children
@@ -11,15 +13,20 @@ public class AssignCookies {
      * @return
      */
     public int findContentChildren(int[] g, int[] s) {
+        // 两者都从小到大排序
         Arrays.sort(g);
         Arrays.sort(s);
 
         int gi = 0, si = 0;
+        // 同时正向遍历
         while (gi < g.length && si < s.length) {
             // 满足贪心规则
+            // 当前能满足的
             if (g[gi] <= s[si]) {
+                // 被满足的+1
                 gi++;
             }
+            //无论满足还是没满足都要+1
             si++;
         }
         return gi;
