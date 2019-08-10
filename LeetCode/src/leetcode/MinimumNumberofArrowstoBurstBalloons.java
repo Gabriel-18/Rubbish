e1ec50056b2d87c459e5dc8aef93fcba9c122248
Kelper <644050173@qq.com>
2019-08-10 11:11:47 +0800

    add some new comments

--- LeetCode/src/leetcode/MinimumNumberofArrowstoBurstBalloons.java
+++ LeetCode/src/leetcode/MinimumNumberofArrowstoBurstBalloons.java
@@ -8,6 +8,7 @@ public class MinimumNumberofArrowstoBurstBalloons {
      * 452. Minimum Number of Arrows to Burst Balloons
      * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
      * 计算不重叠区间
+     * 有多少个重叠区间就需要射多少次
      * @param points
      * @return
      */
@@ -15,9 +16,10 @@ public class MinimumNumberofArrowstoBurstBalloons {
         if (points.length == 0) {
             return 0;
         }
-        //
-        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
+        // 从结尾结尾下的开始
+        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));//升序
         int cnt = 1, end = points[0][1];
+        // 统计重叠区间
         for (int i = 1; i < points.length; i++) {
             if (points[i][0] <= end) {
                 continue;
