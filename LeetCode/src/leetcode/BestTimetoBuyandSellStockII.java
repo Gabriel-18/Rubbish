5e3e1095d27add682e5beba25113661bffcfc188
Kelper <644050173@qq.com>
2019-08-12 19:08:35 +0800

    add some new comments

--- LeetCode/src/leetcode/BestTimetoBuyandSellStockII.java
+++ LeetCode/src/leetcode/BestTimetoBuyandSellStockII.java
@@ -5,12 +5,18 @@ public class BestTimetoBuyandSellStockII {
      * 122. Best Time to Buy and Sell Stock II
      * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
      * 拿前后两天做差,有的赚就交易
+     * 有收益就做？
+     * 对于 [a, b, c, d]，如果有 a <= b <= c <= d ，那么最大收益为 d - a。
+     * 而 d - a = (d - c) + (c - b) + (b - a) ，
+     * 因此当访问到一个 prices [i] 且 prices [i] - prices [i-1] > 0，
+     * 那么就把 prices [i] - prices [i-1] 添加到收益中
      * @param prices
      * @return
      */
     public int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
+            // 可以赚钱就做 梭哈
             if (prices[i] > prices[i-1]) {
                 profit += prices[i] - prices[i-1];
             }
