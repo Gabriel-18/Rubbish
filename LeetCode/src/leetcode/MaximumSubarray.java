2fecdf262df0736269bb572e1127324b6f6ab50f
Kelper <644050173@qq.com>
2019-08-13 14:56:42 +0800

    add some new comments

--- LeetCode/src/leetcode/MaximumSubarray.java
+++ LeetCode/src/leetcode/MaximumSubarray.java
@@ -37,4 +37,19 @@ public class MaximumSubarray {
         }
         return res;
     }
+
+    public int maxSubArray3(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int preSum = nums[0];
+        int maxSum = preSum;
+        for (int i = 1; i < nums.length; i++) {
+            preSum = preSum > 0 ? preSum + nums[i] : nums[i];
+            maxSum = Math.max(maxSum, preSum);
+        }
+        return maxSum;
+    }
+
 }
