f6eb9180a09322fd2edf18bf1fea1e71002737b7
kelper <644050173@qq.com>
2020-01-05 19:02:25 +0800

    add some explanatons

--- LeetCode/src/other/和为S的连续正数序列.java
+++ LeetCode/src/other/和为S的连续正数序列.java
@@ -2,6 +2,15 @@ package other;
 
 import java.util.ArrayList;
 
+
+/*
+用两个数字start和end分别表示序列的最小值和最大值，
+首先将start初始化为1，end初始化为2.
+因为我是从小开始遍历的
+如果从start到end的和大于s，我们就从序列中去掉较小的值(即增大start),
+相反，只需要增大end。
+终止条件为：一直增加start到(1+sum)/2并且end小于sum为止
+*/
 public class 和为S的连续正数序列 {
     public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
         ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
@@ -15,12 +24,15 @@ public class 和为S的连续正数序列 {
             } else if (curSum < sum) {
                 end++;
                 curSum += end;
+                // curSum == sum
             } else {
                 ArrayList<Integer> list = new ArrayList<>();
                 for (int i = start; i <= end; i++) {
                     list.add(i);
                 }
                 ret.add(list);
+                // 下一轮的开始
+                // 更改状态
                 curSum -= start;
                 start++;
                 end++;
