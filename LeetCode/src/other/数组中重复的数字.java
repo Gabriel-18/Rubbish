9bfe4f03238971e7a8a9d023ada648cdaac859ae
kelper <644050173@qq.com>
2020-01-22 12:40:57 +0800

    add new comments and test case

--- LeetCode/src/other/数组中重复的数字.java
+++ LeetCode/src/other/数组中重复的数字.java
@@ -1,6 +1,14 @@
 package other;
 
+import org.junit.Test;
+
 public class 数组中重复的数字 {
+
+    @Test
+    public void test() {
+        int[] nums = {2, 3, 1, 0, 2, 5};
+        duplicate(nums, nums.length, new int[2]);
+    }
     // Parameters:
     //    numbers:     an array of integers
     //    length:      the length of array numbers
@@ -14,12 +22,18 @@ public class 数组中重复的数字 {
             return false;
         }
         for (int i = 0; i < length; i++) {
+            // // 没有重复的情况 都会有交换行为
             while (numbers[i] != i) {
                 // 必然重复的情况
+                // 后面存在重复的数
                 if (numbers[i] == numbers[numbers[i]]) {
+                    // 保存结果
+                    // 并且返回
                     duplication[0] = numbers[i];
                     return true;
                 }
+                // 没有重复的情况 都会有交换行为
+                // 通过交换制造重复
                 swap(numbers, i, numbers[i]);
             }
         }
