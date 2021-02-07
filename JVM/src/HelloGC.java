34e431f980222e6923663d035b06b0c17338f8c4
Gabriel <644050173@qq.com>
2021-02-08 00:10:59 +0800

    init

--- JVM/src/HelloGC.java
+++
@@ -1,7 +0,0 @@
-public class HelloGC {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("****************HelloGC");
-
-        Thread.sleep(Integer.MAX_VALUE);
-    }
-}
--- JVM/src/T.java
+++
@@ -1,8 +0,0 @@
-public class T {
-    public static void main(String[] args) {
-        int a = 100;
-        int b = 200;
-        int retValue = a + b;
-        System.out.println("*******************retValue: " + retValue);
-    }
-}
